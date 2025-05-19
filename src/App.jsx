3d5939cbc0432b4c9ad455901f45b49630bd3e6a
POSSYBERRYGATIEN <gatienpossyberry@gmail.com>
2025-05-19 16:47:53 +0200

    Correction des conflits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Services from './pages/Services';
-import About from './pages/about';  // <-- Correction ici
+import About from './pages/about';
 import Testimonials from './pages/Testimonials';
 import Contact from './pages/Contact';
 import QuoteRequest from './pages/QuoteRequest';
@@ -12,6 +12,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
+  // Vérification WebGL
+  useEffect(() => {
+    function isWebGLAvailable() {
+      try {
+        const canvas = document.createElement('canvas');
+        return !!window.WebGLRenderingContext && !!(canvas.getContext('webgl') || canvas.getContext('experimental-webgl'));
+      } catch  {
+        return false;
+      }
+    }
+
+    if (!isWebGLAvailable()) {
+      console.error("Votre navigateur ne supporte pas WebGL.");
+    } else {
+      console.log("WebGL est activé.");
+    }
+  }, []);
+
   return (
     <Router>
       <Header />
