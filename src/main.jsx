318371ea164410ccf41cea4bf75acde1bc754ae2
POSSYBERRYGATIEN <gatienpossyberry@gmail.com>
2025-05-19 17:19:14 +0200

    Correction des conflits

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+// Vérifie si le navigateur prend en charge les Service Workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  import.meta.env.MODE === 'development' ? (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  ) : (
     <App />
-  </StrictMode>,
-)
+  )
+);
